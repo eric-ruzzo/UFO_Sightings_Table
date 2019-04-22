143ef3847179610300023a0e2d97baaa81f5b23f
Ruzzo <eric.ruzzo@gmail.com>
2019-04-22 19:57:04 -0400

    populated data table

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,24 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Use d3 to select the table
+var table = d3.select("table");
+
+// Use d3 to select the table body
+var tbody = d3.select("tbody");
+
+// Loop through data with forEach to find values for table
+tableData.forEach((sighting) => {
+    console.log(sighting);
+        
+    // Append a table row for each set of data
+    var row = tbody.append("tr");
+
+    Object.entries(sighting).forEach(([key,value]) => {
+    
+        // Add data in each cell in new row
+        var cell = tbody.append("td");
+        cell.text(value);
+    });
+});
