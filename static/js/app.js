7219bd63ed7d45b1a4b53a3feeef4222aa3ecf23
Ruzzo <eric.ruzzo@gmail.com>
2019-04-23 18:32:21 -0400

    committing changes

--- static/js/app.js
+++ static/js/app.js
@@ -11,15 +11,36 @@ var tbody = d3.select("tbody");
 
 // Loop through data with forEach to find values for table
 tableData.forEach((sighting) => {
-    console.log(sighting);
         
-    // Append a table row for each set of data
+    // Append a table row for each data array
     var row = tbody.append("tr");
 
+    // Loop through each key value pair in each array
     Object.entries(sighting).forEach(([key,value]) => {
     
-        // Add data in each cell in new row
+        // Add each value in cell in new row
         var cell = tbody.append("td");
         cell.text(value);
     });
+});
+
+// Select "filter table" button and store as a variable
+var submit = d3.select("#filter-btn");
+
+// Create a function that runs when button is clicked
+submit.on("click", function() {
+
+    // Prevent page from refreshing
+    d3.event.preventDefault();
+
+    // Select input element
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of input element
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+
+    // Filter data to match input value
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    console.log(filteredData);
 });
